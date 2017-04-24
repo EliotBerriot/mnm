2a6289c2fb73fe0ff3adf01bee4bb823264720a2
Eliot Berriot <contact@eliotberriot.com>
2017-04-25 01:00:06 +0200

    Missing import

--- mnm/statuses/management/commands/record_public_timeline.py
+++ mnm/statuses/management/commands/record_public_timeline.py
@@ -14,13 +14,13 @@ class Command(BaseCommand):
         while True:
             print('Initiating connection...')
             try:
-                client = client.get_client(
+                c = client.get_client(
                     client_id=settings.STATUSES_RECORD['client_id'],
                     client_secret=settings.STATUSES_RECORD['client_secret'],
                     access_token=settings.STATUSES_RECORD['access_token'],
                     api_base_url=settings.STATUSES_RECORD['api_base_url'],
                 )
-                record.record_public_status(client)
+                record.record_public_status(c)
             except ValueError as e:
                 print(e)
                 print('Will reconnect in 10 seconds...')
